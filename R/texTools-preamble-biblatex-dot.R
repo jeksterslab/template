f396545eee662ecef6b1759e0139db69ba99452b
jeksterslab <learn.jeksterslab@gmail.com>
2022-04-28 06:22:09 +0000

    Automated build.

--- R/texTools-preamble-biblatex-dot.R
+++ R/texTools-preamble-biblatex-dot.R
@@ -22,7 +22,7 @@
 #' @returns Character string.
 #'
 #' @family Bibliography Functions
-#' @keywords texTools format
+#' @keywords internal
 .PreambleBiblatex <- function(path = "bib",
                               style = "apa",
                               sortcites = TRUE,
